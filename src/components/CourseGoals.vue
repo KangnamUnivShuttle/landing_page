45e97f148bbcadc8fe8f9b0fe7b6862d38abeeb9
Kim HyunWoo <stories2@naver.com>
2022-04-25 01:33:37 +0900

    Update CourseGoals.vue

--- src/components/CourseGoals.vue
+++ src/components/CourseGoals.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <goals-list :goals="filteredGoals"></goals-list>
-    <add-goal @add-goal="addText"></add-goal>
+    <goals-list :goals="context.filteredGoals"></goals-list>
+    <add-goal @add-goal="context.addText"></add-goal>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+// https://stackoverflow.com/a/65027891/7270469
+import { Options, Vue, setup } from "vue-class-component";
+import { ref, computed } from "vue";
 import AddGoal from "./AddGoal.vue";
 import GoalsList from "./GoalsList.vue";
 import { GoalModel } from "../interfaces/Goal.model";
@@ -18,25 +20,52 @@ import { GoalModel } from "../interfaces/Goal.model";
   },
 })
 export default class CourseGoals extends Vue {
-  goals: GoalModel[] = [{ id: "asdf", text: " asdfsaf" }];
+  context = setup(() => {
+    const goals = ref([] as GoalModel[]);
 
-  get filteredGoals(): GoalModel[] {
-    return this.goals.filter(
-      (goal) => !goal.text.includes("angular") && !goal.text.includes("react")
-    );
-  }
+    const filteredGoals = computed(function () {
+      return goals.value.filter(
+        (goal) => !goal.text.includes("angular") && !goal.text.includes("react")
+      );
+    });
 
-  addText(text: string) {
-    const newGoal = {
-      id: new Date().toISOString(),
-      text,
+    function addGoal(text: string) {
+      console.log("test", text);
+      const newGoal = {
+        id: new Date().toISOString(),
+        text,
+      };
+      goals.value.push(newGoal);
+    }
+
+    return {
+      filteredGoals,
+      addText: addGoal,
     };
-    this.goals.push(newGoal);
-  }
+  });
 
   mounted() {
-    console.log("test", this.filteredGoals);
+    console.log("setup", this.context.filteredGoals);
   }
+  //   goals: GoalModel[] = [{ id: "asdf", text: " asdfsaf" }];
+
+  //   get filteredGoals(): GoalModel[] {
+  //     return this.goals.filter(
+  //       (goal) => !goal.text.includes("angular") && !goal.text.includes("react")
+  //     );
+  //   }
+
+  //   addText(text: string) {
+  //     const newGoal = {
+  //       id: new Date().toISOString(),
+  //       text,
+  //     };
+  //     this.goals.push(newGoal);
+  //   }
+
+  //   mounted() {
+  //     console.log("test", this.filteredGoals);
+  //   }
 }
 </script>
 
