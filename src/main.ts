5e61a7cc0e1802f7aa63fc08d6202eaa7f3e8c47
Kim HyunWoo <stories2@naver.com>
2022-05-18 13:30:16 +0900

    asdf

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+import BootstrapVue3 from "bootstrap-vue-3";
+
+// Optional, since every component import their Bootstrap functionality
+// the following line is not necessary
+// import 'bootstrap' asdfasd
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 
 loadFonts();
 
-createApp(App).use(router).use(vuetify).mount("#app");
+createApp(App).use(router).use(BootstrapVue3).use(vuetify).mount("#app");
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,6 +4,19 @@ import "vuetify/styles";
 
 // Vuetify
 import { createVuetify } from "vuetify";
+import { mdi } from "vuetify/iconsets/mdi";
+import { aliases, fa } from "vuetify/iconsets/fa";
 
-export default createVuetify();
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
