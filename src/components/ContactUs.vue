69bc2f965c2926d9034035fad7fa45c8f547ef29
Kim HyunWoo <stories2@naver.com>
2022-05-19 09:11:00 +0900

    Update ContactUs.vue

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,5 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <v-row>
+      <v-col style="height: 100px"></v-col>
+    </v-row>
+    <v-row>
+      <v-col style="text-align: center; margin-top: 15px">
+        <h2>Contact Us</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col style="text-align: center"> stories2@gapmoe.net </v-col>
+    </v-row>
+    <v-row>
+      <v-col style="text-align: center">
+        <a href="https://github.com/KangnamUnivShuttle">
+          <img
+            src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"
+          />
+        </a>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col style="height: 100px"></v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
